85d52c675070ee482d12abac585afc49ef63152b
Nathan Barros <nathanbarrosnt@gmail.com>
2023-05-15 17:40:44 -0300

    Custom game replacement endpoint

--- src/main/java/com/devnatao/dslist/controllers/GameListController.java
+++ src/main/java/com/devnatao/dslist/controllers/GameListController.java
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devnatao.dslist.dto.GameListDTO;
 import com.devnatao.dslist.dto.GameMinDTO;
+import com.devnatao.dslist.dto.ReplacementDTO;
 import com.devnatao.dslist.services.GameListService;
 import com.devnatao.dslist.services.GameService;
 
@@ -36,4 +39,11 @@ public class GameListController {
 		List<GameMinDTO> response = gameService.findByList(listId);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
+	
+	@PostMapping(value = "/{listId}/replacement")
+	public ResponseEntity<?> move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/devnatao/dslist/services/GameListService.java
+++ src/main/java/com/devnatao/dslist/services/GameListService.java
@@ -8,17 +8,49 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devnatao.dslist.dto.GameListDTO;
 import com.devnatao.dslist.entities.GameList;
+import com.devnatao.dslist.projections.GameMinProjection;
 import com.devnatao.dslist.repositories.GameListRepository;
+import com.devnatao.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
-	private GameListRepository repository;
+	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() { 
-		List<GameList> response = repository.findAll();
+		List<GameList> response = gameListRepository.findAll();
 		return response.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		// search all games from game list
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		/**
+		 *  "game" is the element that I want to change
+		 *  the position on game list, start removing it from list
+		 *  using game id (sourceIndex)  
+		 */
+		GameMinProjection game = list.remove(sourceIndex);
+		// then put the same game that was removed, but now on the destinationIndex
+		list.add(destinationIndex, game);
+		// returns the min value between source and destination
+		int min = sourceIndex < destinationIndex ? sourceIndex: destinationIndex;
+		// returns the max value between source and destination
+		int max = sourceIndex > destinationIndex ? sourceIndex: destinationIndex;
+		/*
+		 * updating all the games between min and max, using the listId an then
+		 * list.get(i).getId() to return the game ID e setting the new position 
+		 * to i value
+		 */
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
